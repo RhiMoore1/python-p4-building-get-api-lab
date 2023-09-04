ef3c6e618d67eba88127f2acda21413329b19709
Rhiannon Moore <rhiannonmoore10@yahoo.com>
2023-09-04 12:10:25 -0700

    completes lab

--- server/app.py
+++ server/app.py
@@ -14,25 +14,85 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
+
 @app.route('/')
 def index():
     return '<h1>Bakery GET API</h1>'
 
+
+
+
+# Flask application in flask_app.py has a resource available at "/bakeries"
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    # strategy for serialization
+    bakeries = Bakery.query.all()
+    bakeries_serialized = [bakery.to_dict() for bakery in bakeries]
+    # Flask application in flask_app.py returns JSON representing models.Bakery objects.
+    response = make_response(
+        jsonify(bakeries_serialized),
+        200
+    )
+    # Flask application in flask_app.py provides a response content type of application/json at "/bakeries"
+    response.headers['Content-Type'] = 'application/json'
+    return response
 
+# Flask application in flask_app.py has a resource available at "/bakeries/<int:id>
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    # strategy for serialization
+    bakery = Bakery.query.filter_by(id=id).first()
+    bakery_dict = bakery.to_dict()
+    # Flask application in flask_app.py returns JSON representing one models.Bakery object
+    response = make_response(
+        jsonify(bakery_dict),
+        200
+    )
+    # Flask application in flask_app.py provides a response content type of application/json at "/bakeries/<int:id>"
+    response.headers['Content-Type'] = 'application/json'
+    return response
 
+
+
+
+# Flask application in flask_app.py has a resource available at "/baked_goods/by_price"
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
 
+    baked_goods_by_price = BakedGood.query.order_by(BakedGood.price).all()
+    baked_goods_by_price_dict = [
+        baked_good.to_dict() for baked_good in baked_goods_by_price
+    ]
+    # Flask application in flask_app.py returns JSON representing one models.Bakery object.
+    response = make_response(
+        jsonify(baked_goods_by_price_dict),
+        200
+    )
+    # Flask application in flask_app.py provides a response content type of application/json at "/baked_goods/by_price" 
+    response.headers['Content-Type'] = 'application/json'
+    return response
+
+
+
+
+# Flask application in flask_app.py has a resource available at "/baked_goods/most_expensive".
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+
+    most_expensive = BakedGood.query.order_by(BakedGood.price.desc()).limit(1).first()
+    most_expensive_dict = most_expensive.to_dict()
+    # Flask application in flask_app.py returns JSON representing one models.BakedGood object
+    response = make_response(
+        jsonify(most_expensive_dict),
+        200
+    )
+    # Flask application in flask_app.py provides a response content type of application/json at "/bakeries/<int:id>" 
+    response.headers['Content-Type'] = 'application/json'
+    return response
+
+
+
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -2,19 +2,50 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 from sqlalchemy_serializer import SerializerMixin
 
+
 metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
 })
-
+# We have two models, bakeries and baked goods, in a one-to-many relationship
 db = SQLAlchemy(metadata=metadata)
+# Update the Bakery and BakedGood models to set up the correct associations based on the structure of the tables.
+# Use the relationship() SQLAlchemy method and SQLAlchemy-serializer's SerializerMixin class.
+# Bakery model in models.py can be instantiated with a name
+# Bakery model in models.py can create records that can be committed to the database.
+# Bakery model in models.py can be used to retrieve records from the database.
+# Bakery model in models.py can create records with a to_dict() method for serialization.
+# Bakery model in models.py can delete its records.
 
 class Bakery(db.Model, SerializerMixin):
     __tablename__ = 'bakeries'
 
+    serialize_rules = ('-baked_goods.bakery',)
+
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    baked_goods = db.relationship('BakedGood', backref='bakery')
+
+
+# Update the Bakery and BakedGood models to set up the correct associations based on the structure of the tables.
+# BakedGood model in models.py can be instantiated with a name and price.
+# BakedGood model in models.py can create records that can be committed to the database.
+# BakedGood model in models.py can be used to retrieve records from the database. 
+# BakedGood model in models.py can create records with a to_dict() method for serialization.
+# BakedGood model in models.py can delete its records.
 
 class BakedGood(db.Model, SerializerMixin):
     __tablename__ = 'baked_goods'
 
+    serialize_rules = ('-bakery.baked_goods',)
+
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    price = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    bakery_id = db.Column(db.Integer, db.ForeignKey('bakeries.id'))
     
